7fbcc18aac83a6b8dd473645b143a7948b0f24cc
Your Name <you@example.com>
2018-04-02 13:34:33 +0530

    Added String permutation algorithm

---
+++ RecursionAndBackTracking/StrigPermutation/StringPermutationClient.java
@@ -0,0 +1,8 @@
+package RecursionAndBackTracking.StrigPermutation;
+
+public class StringPermutationClient {
+    public static void main(String[] args) {
+        char input[] = new char[]{'A', 'A', 'B', 'C'};
+        new StringPermutation().permute(input);
+    }
+}
