6ba88d6446c62f33094c2d4dc8e3089c7dfe5a0b
Rituraj Gupta <you@example.com>
2018-04-05 21:21:38 +0530

    Tree Data Structure added delete method

--- Tree/BinaryTree/BinarySearchTree.java
+++ Tree/BinaryTree/BinarySearchTree.java
@@ -7,9 +7,8 @@ public class BinarySearchTree {
     root = null;
   }
 
-
   public void insertData(int data) {
-    insertData(root, data);
+      root = insertData(root, data);
   }
 
   private BTreeNode insertData(BTreeNode root, int data) {
@@ -26,13 +25,55 @@ public class BinarySearchTree {
     return root;
   }
 
+    public void deleteData(int data) {
+        root = deleteDataNode(root, data);
+    }
+
+    private BTreeNode deleteDataNode(BTreeNode root, int data) {
+        if (root == null) {
+            System.out.println("No Data found");
+        } else if (data < root.getData()) {
+            root.setLeft(deleteDataNode(root.getLeft(), data));
+        } else if (data > root.getData()) {
+            root.setRight(deleteDataNode(root.getRight(), data));
+        } else {
+            if (root.getLeft() != null && root.getRight() != null) {
+                BTreeNode temp = findMin(root.getLeft());
+                root.setData(temp.getData());
+                root.setLeft(deleteDataNode(root.getLeft(), root.getData()));
+            } else {
+                BTreeNode temp = root;
+                if (root.getLeft() == null) {
+                    root = root.getRight();
+                }
+                if (root != null && root.getRight() == null) {
+                    root = root.getLeft();
+                }
+            }
+        }
+
+        return root;
+    }
+
+    private BTreeNode findMin(BTreeNode root) {
+        if (root == null) {
+            return null;
+        } else {
+            if (root.getLeft() == null) {
+                return root;
+            } else {
+                return findMin(root.getLeft());
+            }
+        }
+    }
+
   public void displayTree() {
     displayTree(root);
   }
 
   public void displayTree(BTreeNode root) {
     if (root != null) {
-      System.out.println(root.getData() + "  ");
+        System.out.print(root.getData() + "  ");
       displayTree(root.getLeft());
       displayTree(root.getRight());
     }
