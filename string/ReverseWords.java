79e045c359de74b749f8cad0b29961b8324398db
Rituraj Gupta <riturajkbd@gmail.com>
2018-12-30 12:41:01 +0530

    added new methods of tree module
    Signed-off-by: Rituraj Gupta <riturajkbd@gmail.com>

--- string/ReverseWords.java
+++ string/ReverseWords.java
@@ -4,13 +4,16 @@ import java.util.Arrays;
 
 public class ReverseWords {
   public static void main(String[] args) {
+    //    String word = "i.like.this.program.very.much";
     String word = "i.like.this.program.very.much";
-    String reveresed = reveresedWords(word);
+    if (reveresedWords(word) != null) {
+      System.out.println(Arrays.toString(reveresedWords(word)));
+    }
   }
 
-  private static String reveresedWords(String word) {
+  private static String[] reveresedWords(String word) {
     String[] strArray = word.split(".");
-    System.out.println(word.split("."));
-    return "";
+    //    System.out.println(word.split("."));
+    return strArray;
   }
 }
---
+++ string/TestAnagram.java
@@ -0,0 +1,9 @@
+package string;
+
+public class TestAnagram {
+  public static void main(String[] args) {
+    String s1 = "AnGel";
+    String s2 = "Angle";
+    System.out.println(Anagram.isAnagram(s1, s2));
+  }
+}
