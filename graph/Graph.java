5e113d06c31eef447aa052360f2398cd622c2c42
Rituraj Gupta <guptamaharaj@gmail.com>
2025-02-22 18:57:49 +0530

    Learning: added a new graph code for DFS and no of provinces

--- graph/Graph.java
+++ graph/Graph.java
@@ -1,11 +1,13 @@
 package graph;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Graph {
     private int V;
     private List<List<Integer>> adj;
+    private static int timer = 0;
 
     public Graph(int v) {
         this.V = v;
@@ -17,6 +19,7 @@ public class Graph {
 
     public void addEdge(int from, int to) {
         adj.get(from).add(to);
+        adj.get(to).add(from); //for undirected graph
     }
 
     public boolean isCycleUtil(int node, boolean[] visited, boolean[] recStack, List<List<Integer>> adj) {
@@ -54,37 +57,93 @@ public class Graph {
         return !visited[node] && isCycleUtil(node, visited, recStack, adj);
     }
 
+    void bridgeUtil(int u, boolean[] visited, int[] discovery, int[] low, int[] parent) {
+        visited[u] = true;
+        discovery[u] = low[u] = ++timer;
+        for (int v : adj.get(u)) {
+            if (!visited[v]) {
+                parent[v] = u;
+                bridgeUtil(v, visited, discovery, low, parent);
+                low[u] = Math.min(low[u], low[v]);
+                if (low[v] > low[u]) {
+                    System.out.println("bridge found: " + u + "-->" + v);
+                }
+            } else if (v != parent[u]) {
+                low[u] = Math.min(low[u], discovery[v]);
+            }
+        }
+    }
+
+    void findBridges() {
+        boolean[] visited = new boolean[V];
+        int[] discovery = new int[V];
+        int[] low = new int[V];
+        int[] parent = new int[V];
+        Arrays.fill(parent, -1);
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                bridgeUtil(i, visited, discovery, low, parent);
+            }
+
+        }
+    }
+
+    void dfsTraversal() {
+        boolean[] visited = new boolean[V];
+        dfs(7, visited, adj);
+    }
+    //returns no of provinces in graph
+
     /**
-     * 0->1
-     * 1->2
-     * 2->3
-     * 2->4
-     * 3->4
-     * 4->5
-     * 5->6
+     * run for these
+     * graph.addEdge(1, 2);
+     * graph.addEdge(2, 3);
+     * graph.addEdge(4, 5);
+     * graph.addEdge(5, 6);
+     * graph.addEdge(7, 8);
+     *
+     * @return
      */
+    public int noOfProvinces() {
+        int counter = 0;
+        boolean[] visited = new boolean[V];
+        for (int i = 1; i < V; i++) {
+            if (!visited[i]) {
+                counter++;
+                dfs(i, visited, adj);
+            }
+        }
+        return counter;
+    }
+
+    private void dfs(int node, boolean[] visited, List<List<Integer>> adj) {
+        visited[node] = true;
+        System.out.println("visited the node: " + node);
+        for (int v : adj.get(node)) {
+            if (!visited[v]) {
+                dfs(v, visited, adj);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        Graph graph = new Graph(6);
-        graph.addEdge(0, 1);
-        graph.addEdge(1, 2);
-        graph.addEdge(2, 3);
-        graph.addEdge(3, 2);
-        graph.addEdge(2, 4);
-//        graph.addEdge(4, 2);
-        graph.addEdge(3, 4);
-        graph.addEdge(4, 5);
+        Graph graph = new Graph(9);
+
         /*if (graph.isCycle()) {
             System.out.println("Cycle is present");
         } else {
             System.out.println("Cycle not detected");
         }*/
-        for (int i = 0; i < graph.V; i++) {
-            if (graph.isNotSafeNode(i)) {
-                System.out.println(i + " is not a Safe node");
-            } else {
-                System.out.println(i + " is a Safe node");
-            }
-        }
+//        graph.findBridges();
+//        for (int i = 0; i < graph.V; i++) {
+//            if (graph.isNotSafeNode(i)) {
+//                System.out.println(i + " is not a Safe node");
+//            } else {
+//                System.out.println(i + " is a Safe node");
+//            }
+//        }
+//        graph.dfsTraversal();
+        System.out.println("no of provinces in the graph: " + graph.noOfProvinces());
     }
 
 }
--- string/Anagram.java
+++ string/Anagram.java
@@ -1,32 +1,32 @@
-package string;
-
-//import com.sun.istack.internal.NotNull;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.logging.Logger;
-
-public class Anagram {
-  private static Logger logger = Logger.getLogger("Anagram.class");
-  String name;
-
-  public static boolean isAnagram(@NotNull String word, @NotNull String angrm) {
-    /*
-     * if length of both string does not match then obviously both are different
-     */
-    if (word.length() != angrm.length()) return false;
-    for (int i = 0; i < word.length(); i++) {
-      char c = Character.toLowerCase(word.charAt(i));
-      int index =
-          angrm.indexOf(c) != -1 ? angrm.indexOf(c) : angrm.indexOf(Character.toUpperCase(c));
-      if (index != -1) {
-        // if character in 'word' found in 'angrm' remove that character from 'angrm'
-        angrm = angrm.substring(0, index) + angrm.substring(index + 1, angrm.length());
-      } else return false;
-    }
-    /* if all character from 'word' is found in 'angrm'
-     * and even after removing those character from 'angrm'
-     * anagram is not empty then there might be some character
-     * which are not in 'word' so return false in that case */
-    return angrm.isEmpty();
-  }
-}
+//package string;
+//
+////import com.sun.istack.internal.NotNull;
+//import org.jetbrains.annotations.NotNull;
+//
+//import java.util.logging.Logger;
+//
+//public class Anagram {
+//  private static Logger logger = Logger.getLogger("Anagram.class");
+//  String name;
+//
+//  public static boolean isAnagram(@NotNull String word, @NotNull String angrm) {
+//    /*
+//     * if length of both string does not match then obviously both are different
+//     */
+//    if (word.length() != angrm.length()) return false;
+//    for (int i = 0; i < word.length(); i++) {
+//      char c = Character.toLowerCase(word.charAt(i));
+//      int index =
+//          angrm.indexOf(c) != -1 ? angrm.indexOf(c) : angrm.indexOf(Character.toUpperCase(c));
+//      if (index != -1) {
+//        // if character in 'word' found in 'angrm' remove that character from 'angrm'
+//        angrm = angrm.substring(0, index) + angrm.substring(index + 1, angrm.length());
+//      } else return false;
+//    }
+//    /* if all character from 'word' is found in 'angrm'
+//     * and even after removing those character from 'angrm'
+//     * anagram is not empty then there might be some character
+//     * which are not in 'word' so return false in that case */
+//    return angrm.isEmpty();
+//  }
+//}
